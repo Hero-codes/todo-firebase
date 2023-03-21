ee15e736e28b69845d494f1caf0b1dc3e01e23c3
Hero-codes <98174971+Hero-codes@users.noreply.github.com>
2023-03-21 21:28:22 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBTZ0iLtfyTsGfPU7CB6o-yqDRBngSnCis",
-    authDomain: "todo-4b73d.firebaseapp.com",
-    projectId: "todo-4b73d",
-    storageBucket: "todo-4b73d.appspot.com",
-    messagingSenderId: "990340360601",
-    appId: "1:990340360601:web:7f6d9418b42fcc2daa0b08",
-    measurementId: "G-7R9TQCRM2J"
+// YOUR FIREBASE CREDENTIALS
 };
 
 
